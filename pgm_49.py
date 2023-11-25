057c1de5fea2b6a91b00468d5d697aefebca94a3
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-25 15:43:30 +0530

    pgm_49.py

--- pgm_49.py
+++ pgm_49.py
@@ -1,13 +1,10 @@
 try:
-    n=int(input("enter the position to be deleted:"))
     with open('filesample.txt', 'r') as fr:
-        lines = fr.readlines()   # reading line by line
-        p = 1                    # pointer for position
-        with open('filesample.txt', 'w') as fw:  # opening in writing mode
+        lines = fr.readlines()
+        with open('filesample.txt', 'w') as fw:
             for line in lines:
-                if p != n:                       # we want to remove 5th line
+                if line.strip('\n') != 'cccccc':
                     fw.write(line)
-                p += 1
     print("Deleted")
 except:
     print("Oops! something error")
