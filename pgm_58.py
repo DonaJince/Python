ff1bd68981dff07fcfa46a334cdc2547cb37643d
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-29 14:39:25 +0530

    Add files via upload

---
+++ pgm_58.py
@@ -0,0 +1,4 @@
+num=int(input("enter a number:"))
+assert num>0,"negative number"
+
+    
---
+++ pgm_59.py
@@ -0,0 +1,13 @@
+from graphics.rectangle import area_rect,perimeter_rect as pr
+from graphics.circle import perimeter_circle as pc,area_circle
+from graphics.dgraphics.cuboid import volume_cuboid as vc,area_cuboid
+from graphics.dgraphics.sphere import volume_sphere,area_sphere as asp
+pr(2,4)
+area_rect(2,4)
+pc(2)
+area_circle(2)
+vc(2,3,4)
+area_cuboid(2,3,4)
+volume_sphere(4)
+asp(4)
+
