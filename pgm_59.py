11572bdcea841e3b38ed787fa1b33e3dcb7d9ef3
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-29 14:58:41 +0530

    pgm_59.py

--- pgm_59.py
+++ pgm_59.py
@@ -1,13 +1,16 @@
-from graphics.rectangle import area_rect,perimeter_rect as pr
+import graphics.rectangle as r
+from graphics.rectangle import perimeter_rect as pr
 from graphics.circle import perimeter_circle as pc,area_circle
 from graphics.dgraphics.cuboid import volume_cuboid as vc,area_cuboid
-from graphics.dgraphics.sphere import volume_sphere,area_sphere as asp
+from graphics.dgraphics.sphere import volume_sphere as vs,area_sphere as asp
+from graphics import circle
+r.area_rect(2,4)
 pr(2,4)
-area_rect(2,4)
-pc(2)
-area_circle(2)
+pc(3)
+circle.area_circle(3)
 vc(2,3,4)
 area_cuboid(2,3,4)
-volume_sphere(4)
+vs(4)
 asp(4)
+
 
